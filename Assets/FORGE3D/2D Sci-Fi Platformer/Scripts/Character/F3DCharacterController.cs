8f6854ed7956bf37c313a29f5a82fc6ca2f25208
Sebastien <sebrobert@taxelco.com>
2018-06-18 12:28:55 -0400

    Git replay character controller input modification

--- Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/Character/F3DCharacterController.cs
+++ Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/Character/F3DCharacterController.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class F3DCharacterController : MonoBehaviour
 {
+    public enum InputType
+    {
+        KEYBOAD_MOUSE,
+        GAMING_CONTROLLER
+    }
+
     // Components
     public Animator Character;
 
@@ -23,6 +29,8 @@ public class F3DCharacterController : MonoBehaviour
     public float GroundCheckCircleSize;
     public float SideCheckDist;
     public float AimTime;
+    public InputType inputType;
+    public string gamepad;
 
     //
     private Rigidbody2D _rb2D;
@@ -74,7 +82,7 @@ public class F3DCharacterController : MonoBehaviour
         }
 
         // Check and apply Player Input
-        _horizontal = Input.GetAxis("Horizontal");
+        _horizontal = Input.GetAxis(gamepad + "Horizontal");
         Character.SetFloat("Horizontal", Mathf.Abs(_horizontal));
         _weaponController.SetFloat("Horizontal", Mathf.Abs(_horizontal));
 
@@ -118,7 +126,7 @@ public class F3DCharacterController : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position - Vector3.up * GroundCheckCircleSize);
 
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown(gamepad + "Jump"))
         {
             if (!_jump && !_doubleJump && _grounded)
             {
@@ -142,11 +150,11 @@ public class F3DCharacterController : MonoBehaviour
 
         // Crouch 
         // Exit crouch state on button up
-        if (Input.GetButtonUp("Crouch"))
+        if (Input.GetButtonUp(gamepad + "Crouch"))
             _crouch = false;
 
         // Enter crouch on button hold, no jump
-        if (Input.GetButton("Crouch") && !_jump && !_doubleJump && !_crouch)
+        if (Input.GetButton(gamepad + "Crouch") && !_jump && !_doubleJump && !_crouch)
             _crouch = true;
 
         // Jump cancels any current crouch state
@@ -181,8 +189,18 @@ public class F3DCharacterController : MonoBehaviour
         aimPos.z = 0;
 
         // Look direction
-        var dir = (aimPos - WeaponSocket.position).normalized;
-        dir.z = 0;
+        Vector3 dir;
+        if (inputType == InputType.KEYBOAD_MOUSE)
+        {
+            dir = (aimPos - WeaponSocket.position).normalized;
+            dir.z = 0;
+        }
+        else
+        {
+            float rightH = Input.GetAxis(gamepad + "Right_Horizontal");
+            float rightV = Input.GetAxis(gamepad + "Right_Vertical");
+            dir = new Vector3(rightH, -rightV, 0);
+        }
 
         // Weapon socket to FX Socket offset
         var currentWeapon = _weaponController.GetCurrentWeapon();
@@ -200,8 +218,20 @@ public class F3DCharacterController : MonoBehaviour
             Debug.DrawLine(Vector3.zero, localOffset * transform.lossyScale.x, Color.yellow);
 
             //  Debug.DrawLine(WeaponSocket.position, currentWeapon.FXSocket.position, Color.yellow);
-            var worldOffset = WeaponSocket.TransformVector(localOffset) - WeaponSocket.right * 5 * Mathf.Sign(dir.x);
-            var weaponDir = (aimPos - (WeaponSocket.position + worldOffset)).normalized;
+            Vector3 weaponDir;
+            if (inputType == InputType.KEYBOAD_MOUSE)
+            {
+                var worldOffset = WeaponSocket.TransformVector(localOffset) - WeaponSocket.right * 5 * Mathf.Sign(dir.x);
+                weaponDir = (aimPos - (WeaponSocket.position + worldOffset)).normalized;
+            }
+            else
+            {
+                float rightH = Input.GetAxis(gamepad + "Right_Horizontal");
+                float rightV = Input.GetAxis(gamepad + "Right_Vertical");
+                weaponDir = new Vector3(rightH, -rightV, 0);
+            }
+
+
             var socketRotation = Quaternion.LookRotation(Vector3.forward,
                 Mathf.Sign(dir.x) * Vector3.Cross(Vector3.forward, weaponDir));
             WeaponSocket.rotation = Quaternion.Lerp(WeaponSocket.rotation, socketRotation, Time.deltaTime * AimTime);
