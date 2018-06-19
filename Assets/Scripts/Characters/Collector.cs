c3b1b535fc72a16b4e44efd4e0524b74b11d75f3
Nicolas Martineau <nmartineau@taxelco.com>
2018-06-19 19:01:04 -0400

    Add collectable prefabs and character collector

---
+++ Assets/Scripts/Characters/Collector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {        
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            pickupWeapon(other.gameObject);
+        }
+    }
+
+    private void pickupWeapon(GameObject weapon) 
+    {
+        Debug.Log("Pick up weapon " + weapon.name);
+    }
+}
