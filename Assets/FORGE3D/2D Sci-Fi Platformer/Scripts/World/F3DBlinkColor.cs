fb73c83dae20cf197f6b465ae2ed01edbf97703d
Sebastien <sebrobert@taxelco.com>
2018-06-23 08:58:43 -0400

    Blinking blue light
    Different blinking mode (pinpong, repeat)
    Lerp length for blinking

--- Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/World/F3DBlinkColor.cs
+++ Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/World/F3DBlinkColor.cs
@@ -4,9 +4,17 @@ using System.Collections;
 
 public class F3DBlinkColor : MonoBehaviour
 {
-
+    
     public Color ColorA, ColorB;
     public float Rate;
+    public float lerpLength = 1f;
+    public LerpFunctions lerpFunctions = LerpFunctions.PingPong;
+
+    public enum LerpFunctions
+    {
+        PingPong,
+        Repeat
+    }
 
     //
     private SpriteRenderer _sprite;
@@ -17,13 +25,20 @@ public class F3DBlinkColor : MonoBehaviour
 	{
 	    _sprite = GetComponent<SpriteRenderer>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    if (!_sprite) return;
-	    _time += Time.deltaTime;
-	    _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.Abs(Mathf.Sin(_time * Rate)));
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_sprite) return;
+        _time += Time.deltaTime;
+        switch (lerpFunctions)
+        {
+            case LerpFunctions.PingPong:
+                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.PingPong(Time.time * Rate, lerpLength));
+                break;
+            case LerpFunctions.Repeat:
+                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.Repeat(Time.time * Rate, lerpLength));
+                break;
+        }
+    }
 }
