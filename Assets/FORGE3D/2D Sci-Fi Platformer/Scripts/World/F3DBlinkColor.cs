34c2d50d6c3b72ae99e95bc9a82a7ec851189979
Sebastien Robert <sebas_robert@msn.com>
2018-06-24 09:48:51 -0400

    Allow random blink

--- Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/World/F3DBlinkColor.cs
+++ Assets/FORGE3D/2D Sci-Fi Platformer/Scripts/World/F3DBlinkColor.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿//using System;
 using UnityEngine;
-using System.Collections;
 
 public class F3DBlinkColor : MonoBehaviour
 {
@@ -8,6 +7,7 @@ public class F3DBlinkColor : MonoBehaviour
     public Color ColorA, ColorB;
     public float Rate;
     public float lerpLength = 1f;
+    public float randomLerpLengthModifier = 0f;
     public LerpFunctions lerpFunctions = LerpFunctions.PingPong;
 
     public enum LerpFunctions
@@ -21,9 +21,12 @@ public class F3DBlinkColor : MonoBehaviour
 
     private float _time;
 
+    private float _lerp;
+
     void Awake()
 	{
 	    _sprite = GetComponent<SpriteRenderer>();
+        _lerp = lerpLength + Random.Range(0f, randomLerpLengthModifier);
 	}
 
     // Update is called once per frame
@@ -31,13 +34,15 @@ public class F3DBlinkColor : MonoBehaviour
     {
         if (!_sprite) return;
         _time += Time.deltaTime;
+
+
         switch (lerpFunctions)
         {
             case LerpFunctions.PingPong:
-                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.PingPong(Time.time * Rate, lerpLength));
+                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.PingPong(Time.time * Rate, _lerp));
                 break;
             case LerpFunctions.Repeat:
-                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.Repeat(Time.time * Rate, lerpLength));
+                _sprite.color = Color.Lerp(ColorA, ColorB, Mathf.Repeat(Time.time * Rate, _lerp));
                 break;
         }
     }
