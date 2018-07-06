0c953f8240b2f9e910768f077f0fc78a5ea307a2
Nicolas Martineau <nmartineau@taxelco.com>
2018-07-06 07:13:37 -0400

    Add the ability to spawn n collectables at a time

--- Assets/Scripts/Collectables/CollectableSpawner.cs
+++ Assets/Scripts/Collectables/CollectableSpawner.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CollectableSpawner : MonoBehaviour {
 
     public GameObject[] collectables = new GameObject[0];
     public Transform[] spawnPoints = new Transform[0];
+    [Range(1, 4)]
+    public int spawnCount;
     public float startWait;
     public float spawnWait;
 
-	// Use this for initialization
-	void Start () {
+    private HashSet<int> spawnPointExclusion = new HashSet<int>();
+
+    void Awake()
+    {
+        // To avoid infine loop
+        Assert.IsTrue(spawnCount <= spawnPoints.Length);
+    }
+
+    // Use this for initialization
+    void Start () {
         if (collectables.Length > 0 && spawnPoints.Length > 0)
         {
             StartCoroutine(SpawnCollectables());
@@ -20,16 +31,44 @@ public class CollectableSpawner : MonoBehaviour {
     IEnumerator SpawnCollectables () {
         yield return new WaitForSeconds(startWait);
 
-        GameObject lastSpawnCollectable = null;
+        GameObject[] collectableObjects = new GameObject[spawnCount];
         while(true) {
-            if(lastSpawnCollectable != null) {
-                Destroy(lastSpawnCollectable); 
+            DestroyCollectables(collectableObjects);
+
+            spawnPointExclusion.Clear();
+            for (int i = 0; i < spawnCount; i++)
+            {
+                int randomSpawnPointIndex = getRandomIntExcept(0, spawnPoints.Length, spawnPointExclusion);
+                spawnPointExclusion.Add(randomSpawnPointIndex);
+                Transform spawnPoint = spawnPoints[randomSpawnPointIndex];
+                GameObject collectable = collectables[Random.Range(0, collectables.Length)];
+                collectableObjects[i] = Instantiate(collectable, spawnPoint.position, spawnPoint.rotation);
             }
-            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-            GameObject collectable = collectables[Random.Range(0, collectables.Length)];
-            lastSpawnCollectable = Instantiate(collectable, spawnPoint.position, spawnPoint.rotation);
 
             yield return new WaitForSeconds(spawnWait);
         }
 	}
+
+    private int getRandomIntExcept(int min, int max, HashSet<int> except)
+    {
+        int randomInt;
+        do
+        {
+            randomInt = Random.Range(min, max);
+        } while (except.Contains(randomInt));
+
+        return randomInt;
+    }
+
+    private void DestroyCollectables(GameObject[] collectableObjects)
+    {
+        for (int i = 0; i < collectableObjects.Length; i++)
+        {
+            GameObject collectable = collectableObjects[i];
+            if (collectable != null)
+            {
+                Destroy(collectable);
+            }
+        }
+    }
 }
